5faf067058c30305de64de20318c37327c9cf4c7
unknown <abhishek.fagna@ceddaac.com>
2024-08-09 18:23:33 +0530

    f

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  private http = inject(HttpClient);
+
+  ngOnInit(): void {
+    this.http
+      .get('https://dummy.restapiexample.com/api/v1/employees')
+      .subscribe({
+        next: (response) => {
+          console.log(response);
+          // handle the response data here
+        },
+        error: (error) => {
+          console.error('There was an error!', error);
+        },
+      });
+  }
   title = 'example';
 }
